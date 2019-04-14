18300b4fdf5384a129e8d486f510ac897d70164f
kfm <1376822651@qq.com>
2019-04-14 18:29:55 +0800

    初始化

---
+++ src/main/java/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package mapper;
+
+/**
+ * @author kfm
+ * @date 2019.02.13 10:47
+ */
+public interface UserMapper {
+    /**
+     * 统计
+     * @return
+     */
+    int countUser();
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,27 @@
+import mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author kfm
+ * @date 2019.02.13 10:49
+ */
+public class UserTest {
+
+    @Test
+    public void testCount() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        int count = userMapper.countUser();
+        System.out.println(count);
+    }
+}
